e1ed855244860733a487394eaf47c3ded8cc709b
M-sandoval-93 <sandoval.mario.08@gmail.com>
2023-03-10 11:53:46 -0300

    modificacion configuracion conexion ddbb

--- config/config.php
+++ config/config.php
@@ -1,10 +1,13 @@
 <?php
 
 // Configuración para la conexión con la base de datos
-const DB_HOST = 'localhost';
-const DB_USSER = 'postgres';
+// const DB_HOST = 'localhost';
+const DB_HOST = '172.16.0.176';
+// const DB_USSER = 'postgres';
+const DB_USSER = 'soporteinformatica';
 const DB_PASSWORD = 'admin';
-const DB_DATA = 'db_liceo';
+// const DB_DATA = 'db_liceo';
+const DB_DATA = 'db_loceo_lvl';
 const DB_CHARSET = 'utf8';
 const DB_PORT = '5432';
 

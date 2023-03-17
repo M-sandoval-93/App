2a713e1d0964b9696f303c22172b1e02b90e2d41
M-sandova-93 <sandoval.mario.08@gmail.com>
2023-03-17 10:06:30 -0300

    quitar btn ok de retraso

--- config/config.php
+++ config/config.php
@@ -1,16 +1,16 @@
 <?php
 
 // Configuración para la conexión con la base de datos
-// const DB_HOST = '172.16.0.176';
-// const DB_USSER = 'soporteinformatica';
-// const DB_DATA = 'db_liceo_lvl';
+const DB_HOST = '172.16.0.176';
+const DB_USSER = 'soporteinformatica';
+const DB_DATA = 'db_liceo_lvl';
 
-const DB_HOST = 'localhost';
-const DB_USSER = 'postgres';
-const DB_DATA = 'db_lvl_local';
-const DB_PASSWORD = 'user2019';
+// const DB_HOST = 'localhost';
+// const DB_USSER = 'postgres';
+// const DB_DATA = 'db_lvl_local';
+// const DB_PASSWORD = 'user2019';
 
-// const DB_PASSWORD = 'admin';
+const DB_PASSWORD = 'admin';
 const DB_CHARSET = 'utf8';
 const DB_PORT = '5432';
 

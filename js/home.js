664aa8a5f84905c5c88d851693ad350fa71f0a0b
M-sandova-93 <sandoval.mario.08@gmail.com>
2023-03-19 15:42:35 -0300

    actualizacion 190323_1540

--- js/home.js
+++ js/home.js
@@ -1,19 +1,5 @@
 import {LibreriaFunciones, generar_dv, spanish } from './librerias/librerias.js';
 let datos = 'comprobarSuspension';
-const meses = {
-    0: "enero",
-    1: "febrero",
-    2: "marzo",
-    3: "abril",
-    4: "mayo",
-    5: "junio",
-    6: "julio",
-    7: "agosto",
-    8: "septiembre",
-    9: "octubre",
-    10: "noviembre",
-    11: "diciembre"
-}
 
 
 // Función para comprobar y actualizar la suspensión de matrículas

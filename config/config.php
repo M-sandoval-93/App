8e8423f738727c4933275344a95b52d174168fe9
M-sandoval-93 <sandoval.mario.08@gmail.com>
2023-03-28 23:24:14 -0300

    actualizacion 280323_2324

--- config/config.php
+++ config/config.php
@@ -1,16 +1,16 @@
 <?php
 
 // Configuración para la conexión con la base de datos
-// const DB_HOST = '172.16.0.92';
-// const DB_USSER = 'adminpglvl';
-// const DB_DATA = 'db_liceo_sistema';
-// const DB_PASSWORD = '@dm1npgLVL';
+const DB_HOST = '172.16.0.92';
+const DB_USSER = 'adminpglvl';
+const DB_DATA = 'db_liceo_sistema';
+const DB_PASSWORD = '@dm1npgLVL';
 
 
-const DB_HOST = 'localhost';
-const DB_USSER = 'postgres';
-const DB_DATA = 'db_liceo';
-const DB_PASSWORD = 'admin';
+// const DB_HOST = 'localhost';
+// const DB_USSER = 'postgres';
+// const DB_DATA = 'db_liceo';
+// const DB_PASSWORD = 'admin';
 
 // const DB_HOST = 'localhost';
 // const DB_USSER = 'postgres';

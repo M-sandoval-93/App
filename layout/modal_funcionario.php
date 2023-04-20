16e3a9f24871a2ba758dd52f4e14ca6fc60d7642
M-sandoval-93 <sandoval.mario.08@gmail.com>
2023-04-20 14:48:54 -0400

    actualizacion 200423_1448_casa

--- layout/modal_funcionario.php
+++ layout/modal_funcionario.php
@@ -18,24 +18,25 @@
                   <div class="text-primary h3" id="cantidad_nuevo_registro">97</div>
                 </div>
             </div>
-            <!-- <div class="col-sm-5 col-md-3">
-              <label for="fecha_ingreso_estudiante" class="form-label">Fecha ingreso <span class="text-danger fs-5">*</span></label>
-              <input type="date" id="fecha_ingreso_estudiante" class="form-control">
-            </div> -->
+
+            <div class="col-sm-5 col-md-3">
+              <label for="fecha_nacimiento_funcionario" class="form-label">Fecha nacimiento <span class="text-danger fs-5">*</span></label>
+              <input type="date" id="fecha_nacimiento_funcionario" class="form-control">
+            </div>
           </div>
 
           <div class="row align-items-center mt-3">
-            <label for="rut_estudiante" class="form-label">Rut <span class="text-danger fs-5">*</span></label>
+            <label for="rut_funcionario" class="form-label">Rut <span class="text-danger fs-5">*</span></label>
             <div class="col-sm-6 col-lg-5">
               <div class="row align-items-center">
                 <div class="col-7 rut">
-                  <input type="text" class="form-control text-center" id="rut_estudiante" required>
+                  <input type="text" class="form-control text-center" id="rut_funcionario" required>
                 </div>
                 <div class="col-1 not_padding text-center">
                   <span>-</span>
                 </div>
                 <div class="col-4 dv_rut">
-                  <input type="text" class="form-control text-center" id="dv_rut_estudiante" disabled>
+                  <input type="text" class="form-control text-center" id="dv_rut_funcionario" disabled>
                 </div>
               </div>
             </div>
@@ -46,105 +47,53 @@
 
           <div class="row g-3 mt-4">
             <div class="col-md-4">
-              <label for="nombre_estudiante" class="form-label">Nombres funcionario <span class="text-danger fs-5">*</span></label>
-              <input type="text" class="form-control" id="nombre_estudiante">
+              <label for="nombre_funcionario" class="form-label">Nombres funcionario <span class="text-danger fs-5">*</span></label>
+              <input type="text" class="form-control" id="nombre_funcionario">
             </div>
             <div class="col-md-4">
-              <label for="ap_estudiante" class="form-label">Apellido paterno <span class="text-danger fs-5">*</span></label>
-              <input type="text" class="form-control" id="ap_estudiante">
+              <label for="ap_funcionario" class="form-label">Apellido paterno <span class="text-danger fs-5">*</span></label>
+              <input type="text" class="form-control" id="ap_funcionario">
             </div>
             <div class="col-md-4">
-              <label for="am_estudiante" class="form-label">Apellido materno <span class="text-danger fs-5">*</span></label>
-              <input type="text" class="form-control" id="am_estudiante">
+              <label for="am_funcionario" class="form-label">Apellido materno <span class="text-danger fs-5">*</span></label>
+              <input type="text" class="form-control" id="am_funcionario">
             </div>
           </div>
 
-          <!-- <div class="row align-items-end">
-            <div class="col-md-5 col-sm-6">
-              <div class="row align-items-center">
-                <label for="justificacion_rut_estudiante" class="form-label">Rut estudiante <span class="text-danger fs-5">*</span></label>
-                <div class="col-8 rut">
-                    <input type="text" class="form-control text-center" id="justificacion_rut_estudiante" required>
-                </div>
-                <div class="col-1 not_padding text-center">
-                    <span>-</span>
-                </div>
-                <div class="col-3 dv_rut">
-                    <input type="text" class="form-control text-center" id="justificacion_dv_rut_estudiante" disabled>
-                </div>
-              </div>
+          <div class="row g-3 mt-4">
+            <div class="col-md-4"> <!-- llenar con ajax -->
+              <label for="tipo_funcionario" class="form-label">Tipo funcionario <span class="text-danger fs-5">*</span></label>
+              <select class="form-select" id="tipo_funcionario">
+                <option selected> -------------- </option>
+                <option value=7> Asistente </option>
+                <option value=8> Docente </option>
+                <option value=8> Docente directivo </option>
+                <option value=1> Paradocente </option>
+              </select>
             </div>
 
-            <div class="col-md-3 col-sm-4">
-              <label for="justificacion_fecha" class="form-label">Fecha </label>
-              <input type="text" class="form-control text-center" id="justificacion_fecha" disabled>
+            <div class="col-md-5"> <!-- llenar con ajax -->
+              <label for="departamento_funcionario" class="form-label">Departamento <span class="text-danger fs-5">*</span></label>
+              <select class="form-select" id="departamento_funcionario">
+                <option selected> -------------- </option>
+                <option value=7> Informática </option>
+                <option value=8> UTP </option>
+                <option value=8> Inspectoría general </option>
+                <option value=1> Orientación </option>
+              </select>
             </div>
+
+            <div class="col-md-3">
+              <label for="sexo_funcionario" class="form-label">Sexo <span class="text-danger fs-5">*</span></label>
+              <select class="form-select" id="sexo_funcionario">
+                <option selected disabled> ------- </option>
+                <option value="M">Masculino</option>
+                <option value="F">Femenina</option>
+              </select>
+            </div>
+
+
           </div>
-
-          <div class="row mt-4">
-            <div class="col-sm-9 col-md-10">
-              <label for="justificacion_nombre_estudiante" class="form-label">Nombre estudiante</label>
-              <input type="text" class="form-control" id="justificacion_nombre_estudiante" disabled>
-            </div>
-            <div class="col-sm-3 col-md-2">
-              <label for="justificacion_curso_estudiante" class="form-label">Curso</label>
-              <input type="text" class="form-control" id="justificacion_curso_estudiante" disabled>
-            </div>
-          </div>
-
-          <div class="row mt-4">
-            <div class="col-sm-5 col-md-3">
-              <label for="justificacion_fecha_inicio" class="form-label">Fecha inicio <span class="text-danger fs-5">*</span></label>
-              <input type="date" id="justificacion_fecha_inicio" class="form-control">
-            </div>
-
-            <div class="col-sm-5 col-md-3">
-              <label for="justificacion_fecha_termino" class="form-label">Fecha termino <span class="text-danger fs-5">*</span></label>
-              <input type="date" id="justificacion_fecha_termino" class="form-control">
-            </div>
-          </div>
-
-          <div class="row mt-4">
-            <div class="col-sm-12">
-              <label for="justificacion_apoderado" class="form-label">Apoderado(a) <span class="text-danger fs-5">*</span></label>
-              <select id="justificacion_apoderado" class="form-select"></select>
-            </div>
-          </div>
-
-          <div class="row mt-4">
-            <div class="col-lg-12">
-              <label for="justificacion_motivo_causa" class="form-label">Motivo/Causa inasistencia</label>
-              <textarea id="justificacion_motivo_causa" class="form-control"></textarea>
-            </div>
-          </div>
-
-          <div class="row mt-4" id="check">
-            <div class="col-6">
-              <div class="form-check">
-                <input type="checkbox" name="" id="justificacion_documento" class="form-check-input" style="margin-top: 12px;">
-                <div class="form-check-label">
-                  <select id="justificacion_tipo_documento" class="form-select form-check-label" disabled>
-                    <option selected value="0">Presenta documento</option>
-                    <option value="1">Certificado médico</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-3">
-              <div class="form-check mt-2">
-                <input type="checkbox" id="justificacion_informacion_verbal" class="form-check-input">
-                <label for="justificacion_informacion_verbal" class="form-check-label">Información verbal</label>
-              </div>
-            </div>
-
-            <div class="col-3">
-              <div class="form-check mt-2">
-                <input type="checkbox" id="justificacion_prueba_pendiente" class="form-check-input" disabled>
-                <label for="justificacion_prueba_pendiente" class="form-check-label">Prueba pendiente</label>
-              </div>
-            </div>
-          </div> -->
         </div>
 
         <div class="modal-footer">
